1732e4e5ea437dd72d5c5dc85254cc00dc2e4be8
tobiwanse <tobiasforslund@icloud.com>
2026-04-15 23:27:54 +0200

    Update deployment configurations for production and staging environments

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,4 @@ server '172.19.2.132', user: 'admin', roles: %w[app web db]
 
 set :branch, 'main'
 set :stage, :production
-
 set :deploy_to, '/Users/admin/www/fkaros-web'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 
 server '172.19.2.132', user: 'admin', roles: %w[app web db]
 
-set :branch, 'develop'
+set :branch, 'main'
 set :stage, :staging
 
 set :deploy_to, '/Users/admin/www/fkaros-web-test'
